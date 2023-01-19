422f4da8c6cafe56aa4e087985c721e4a4a308c2
michaelkeates <mail@michaelkeates.co.uk>
2023-01-19 16:41:30 +0000

    start from scratch

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,65 +1,6 @@
-import { AppShell, Header, MantineProvider } from '@mantine/core'
-import { NotificationsProvider } from '@mantine/notifications'
-import { NhostClient, NhostProvider } from '@nhost/nextjs'
-import { NhostApolloProvider } from '@nhost/react-apollo'
-import { inspect } from '@xstate/inspect'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import NavBar from '../components/NavBar'
-import { BACKEND_URL } from '../helpers'
-import '../styles/globals.css?inline'
 
-const devTools = typeof window !== 'undefined' && !!process.env.NEXT_PUBLIC_DEBUG
-if (devTools) {
-  inspect({
-    url: 'https://stately.ai/viz?inspect',
-    iframe: false
-  })
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-const nhost = new NhostClient({ backendUrl: BACKEND_URL, devTools })
-const title = 'Project eduLab'
-function MyApp({ Component, pageProps }: AppProps) {
-  // * Monorepo-related. See: https://stackoverflow.com/questions/71843247/react-nextjs-type-error-component-cannot-be-used-as-a-jsx-component
-  // const AnyComponent = Component as any
-  return (
-    <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
-      <NhostApolloProvider nhost={nhost}>
-        <Head>
-          <title>{title}</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </Head>
-
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{
-            /** Put your mantine theme override here */
-            colorScheme: 'light'
-          }}
-        >
-          <NotificationsProvider>
-            <AppShell
-              padding="md"
-              navbar={<NavBar />}
-              header={
-                <Header height={60} p="xs">
-                  {title}
-                </Header>
-              }
-              styles={(theme) => ({
-                main: {
-                  backgroundColor:
-                    theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0]
-                }
-              })}
-            >
-              <Component {...pageProps} />
-            </AppShell>
-          </NotificationsProvider>
-        </MantineProvider>
-      </NhostApolloProvider>
-    </NhostProvider>
-  )
-}
-
-export default MyApp
