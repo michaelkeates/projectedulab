1ebad4db129f8e9aace844d0be64de58760d0351
michaelkeates <mail@michaelkeates.co.uk>
2023-02-19 19:06:12 +0000

    Update vncclient.tsx

--- components/vncclient/vncclient.tsx
+++ components/vncclient/vncclient.tsx
@@ -1,50 +1,79 @@
-//components/vncclient/vncclient.tsx
-//import dependencies and types from react-vnc
-import React, { useEffect, useState } from 'react';
-import { VncScreen } from 'react-vnc';
+import React, { useState, useRef } from "react";
+import { VncScreen } from "react-vnc";
 
-//define the props for the vncclient component
-interface VncClientProps {
-  container?: Element;
-  url: string;
+function App() {
+  const [url, setUrl] = useState("wss://192.168.0.103/wsproxy/5702/");
+  const [connected, setConnected] = useState(false);
+  const vncScreenRef = useRef<React.ElementRef<typeof VncScreen>>(null);
+
+  const Spacer = () => (
+    <div style={{ width: "2rem", display: "inline-block" }} />
+  );
+
+  const isValid = (vncUrl: string) => {
+    if (!vncUrl.startsWith("ws://") && !vncUrl.startsWith("wss://")) {
+      return false;
+    }
+
+    return true;
+  };
+
+  return (
+    <>
+      <div style={{ margin: "1rem" }}>
+        <label htmlFor="url">URL for VNC Stream</label>
+        <Spacer />
+
+        <input
+          type="text"
+          onChange={({ target: { value } }) => {
+            setUrl(value);
+          }}
+          name="url"
+          value={url}
+          placeholder="wss://your-vnc-url"
+        />
+      </div>
+
+      <div style={{ opacity: 0.5, margin: "1rem" }}>
+        Since the site is loaded over HTTPS, only `wss://` URLs (SSL encrypted
+        websockets URLs) are supported.
+      </div>
+
+      <div style={{ margin: "1rem" }}>
+        <button
+          onClick={() => {
+            const { connect, connected, disconnect } =
+              vncScreenRef.current ?? {};
+            if (connected) {
+              disconnect?.();
+              return;
+            }
+            connect?.();
+          }}
+        >
+          Connect / Disconnect
+        </button>
+      </div>
+
+      <div style={{ margin: "1rem" }}>
+        {isValid(url) ? (
+          <VncScreen
+            url={url}
+            scaleViewport
+            background="#000000"
+            style={{
+              width: "75vw",
+              height: "75vh",
+            }}
+            debug
+          />
+        ) : (
+          <div>VNC URL not provided.</div>
+        )}
+      </div>
+    </>
+  );
 }
 
-//define the vnclient component
-const VncClient: React.FunctionComponent<VncClientProps> = (
-  props,
-): JSX.Element => {
-  const [url, setUrl] = useState('');
-
-  //useEffect to set the url
-  useEffect(() => {
-    setUrl(props.url);
-  }, [props]);
-
-  //return the vncscreen component
-  //return if the url is not empty
-  return url.length > 0 ? (
-    //parameters for the vncscreen component
-    <>
-      <VncScreen
-        //url={url}
-        url="wss://192.168.0.103/wsproxy/5702/"
-        //url="wss://test.michaelkeates.co.uk/websockify/"
-        scaleViewport={true}
-        background="#000000"
-        resizeSession={true}
-        dragViewport={true}
-        clipViewport={true}
-        style={{
-          width: '100%',
-          //height: '32vh',
-          height: '60vh'
-        }}
-      />
-    </>
-  ) : (
-    <div>VNC URL not provided</div>
-  );
-};
-
-//export the vnclient component
-export default VncClient;
+export default App;
