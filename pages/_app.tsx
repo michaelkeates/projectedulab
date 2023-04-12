21430e14e6e14dc1358ed552effb3b08cae6ba18
michaelkeates <mail@michaelkeates.co.uk>
2023-04-12 19:56:37 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,10 +22,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   // const AnyComponent = Component as any
   return (
     <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
-      <NhostApolloProvider>
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
+      <NhostApolloProvider nhost={nhost}>
+        <NotificationsProvider position="top-right" zIndex={2077}>
+          <ChakraProvider>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </NotificationsProvider>
       </NhostApolloProvider>
     </NhostProvider>
   );
